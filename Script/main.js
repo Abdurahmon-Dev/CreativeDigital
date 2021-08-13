96693b9a73680bd02afed373abde38a91c6e7f80
Abdurahmon-Dev <abdurahmonmirobidov200@gmail.com>
2021-08-13 20:31:31 +0500

    why we?

---
+++ Script/main.js
@@ -0,0 +1,20 @@
+// window.addEventListener('load',() => {
+//     const url_string = location.href
+//     const url = new URL(url_string)
+//     getQueryVariable(url);
+// })
+// function getQueryVariable(url) {
+//     if (url.hash !== '#about-us') {
+//         document.getElementById('galvniy').classList.remove('active')
+//     }
+//     else {
+//         document.getElementById('galvniy').classList.add('active');
+//     }
+//     if (url.hash !== '#mainC') {
+//         document.getElementById('o-nas').classList.remove('active')
+//     }
+//     else {
+//         document.getElementById('o-nas').classList.add('active');
+//     }
+    
+// }
